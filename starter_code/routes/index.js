658aea177b1c4ae4890eed1ace243f0202095837
Helleke Heikkine <helleke.heikkinen@gmail.com>
2019-04-14 00:27:17 +0200

    displays movies

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+    res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+    const Movie = require("../models/Movie.js");
+
+    Movie.find()
+        .then(moviesArray => {
+            res.render("movies", { movies: moviesArray });
+        })
+        .catch(err => {
+            console.log("Error!", err);
+        });
 });
 
 module.exports = router;
